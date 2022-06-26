a2313555090a7ca91bd55510ed0a612101c27988
timon <timonsus@gmail.com>
2022-06-27 04:21:51 +0500

    released list component

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
-import Api from './api/api';
-
-async function getList(): Promise<void> {
-  const api = new Api();
-
-  console.log(await api.getMinMax('area'));
-}
-
-const btn = document.querySelector("button");
-btn?.addEventListener("click", getList);
+import './components/list';
